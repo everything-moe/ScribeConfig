bab40e1c6568d7bbc434bbcab0be7da2807162df
Barinade <poco0317@users.noreply.github.com>
2021-06-11 21:01:28 -0500

    init

---
+++ src/main/java/moe/everything/scribe/config/ScribeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package moe.everything.scribe.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScribeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScribeConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/everything/scribe/config/ScribeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.everything.scribe.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
